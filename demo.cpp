d6896d6d2bf401fb43ef05eae1e54787ddd4acc8
khandelwalshivam644-pixel <khandelwalshivam644@gmail.com>
2026-08-14 00:07:59 +0530

    conditional statement complete

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,64 @@
-#include<iostream>
-int main() {
-    std::cout<<"hey shivam"<<"\n";
-    std::cout<<"hey shivam";
-    return 0;
+// #include<iostream>
+// int main() {
+//     std::cout<<"hey shivam"<<"\n";
+//     std::cout<<"hey shivam";
+//     return 0;
+// #include <iostream>
+// using namespace std ;
+// int main(){
+//     int x;
+//     cin>> x;
+//     cout << "value of x :"<< x;
+
+// }
+
+//if else statement
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cin >> age ;
+//     if(age>=18){
+//         cout<<"you are eligible for vote";
+//     }
+//     else{
+//         cout<<"you are not eligible";
+//     }
+// return 0;
+// }
+
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//     int marks;
+//     c
+// }
+//switch statement
+#include<bits/stdc++.h>
+using namespace  std;
+int main(){
+    int day;
+    cin>>day;
+    switch(day){
+        case 1:
+           cout<<"Monday";
+
+        case 2:
+            cout<<"Tuesday";
+        case 3:
+            cout<<"Wednesday";
+        case 4:
+             cout<<"Thursday";          
+        case 5:
+              cout<<"friday";
+        case 6:
+              cout<<"Satursday";
+        case 7:
+               cout<<"Sunday";
+               break;
+        default:
+               cout<<"invalid ";
+    }
+return 0;
 }
+
