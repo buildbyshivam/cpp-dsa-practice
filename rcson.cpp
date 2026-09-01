81a284775f9da285eab39f9d0fbea4b7f96b3f72
khandelwalshivam644-pixel <khandelwalshivam644@gmail.com>
2026-09-01 22:44:33 +0530

    recursion add and fact

--- rcson.cpp
+++ rcson.cpp
@@ -110,18 +110,60 @@
 //     return 0;
 // }
 // backtracing n to 1
-#include<iostream>
-using namespace std ;
-void f(int i ,int n){
-    if(i>n)
-        return ;
-    f(i+1 , n);
-    cout<<i << endl;
+// #include<iostream>
+// using namespace std ;
+// void f(int i ,int n){
+//     if(i>n)
+//         return ;
+//     f(i+1 , n);
+//     cout<<i << endl;
     
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     f(1 , n);
+//     return 0;
+// }
+
+//summation
+// #include<iostream>
+// using namespace std;
+// void Sum(int i , int sum ){
+//     if(i<1)
+//     cout<<sum<<endl;
+//     return;
+//     Sum(i-1 , sum + i);
+
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     Sum(n , 0);
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int sum(int n ){
+//     if(n==0) return 0;
+//     return n + sum(n-1);
+
+// }
+// int main(){
+//     int n = 5;
+//     cout<<sum(n);
+
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==1) return 1 ;
+    return n * fact(n-1);
 }
 int main(){
     int n;
-    cin>>n;
-    f(1 , n);
+    cin >> n ;
+    cout<<fact(n);
     return 0;
 }
