6d1204ffe20211a0f15762655468f6a10fdb37ff
khandelwalshivam644-pixel <khandelwalshivam644@gmail.com>
2026-09-10 13:33:01 +0530

    check the array is sorted

--- array.cpp
+++ array.cpp
@@ -243,3 +243,32 @@
     
 // return 0;
 // }
+//for finding the sorted array
+#include<bits/stdc++.h>
+using namespace std ;
+bool check_sorted(vector<int> &arr , int n){
+    for(int i= 1; i< n ; i++){
+        if(arr[i] < arr[i-1]){
+            return false;
+        }
+
+    }
+    return true;
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i =0; i<n ; i++){
+        cin >> arr[i] ;
+    }
+    if(check_sorted(arr,n)){
+        cout << " array is sorted";
+    }
+    else{
+        cout << "array is not sorted";
+    }
+        
+    
+return 0;
+}
